5c5d6b83974f600af72d6b9a0554533901981044
houssemradhouane <radhouane.houssem@gmail.com>
2022-04-29 15:22:31 +0200

    first classes

---
+++ Teacher.java
@@ -0,0 +1,97 @@
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Teacher {
+
+     // The teacher Id
+     @Id
+     @GeneratedValue(strategy=GenerationType.IDENTITY)
+     int teacher_id;
+     // The first name of the student
+     String first_name;
+     // The last name of the student
+     String last_name;
+     // The password
+     String password;
+     // The email
+     String email;
+     // the teacher
+     @OneToMany
+     ArrayList<Student> students;
+     // Provided courses
+     @ManyToMany(mappedBy="author", fetch = FetchType.EAGER)
+     ArrayList<Course> courses;
+
+     public Teacher() {
+         super();
+     }
+
+     //defining the getters
+
+    public int get_id() {
+        return this.teacher_id;
+    }
+
+    public String get_firstname() {
+        return this.first_name;
+    }
+
+    public String get_lastname() {
+        return this.last_name;
+    }
+
+    public String get_password() {
+        return this.password;
+    }
+
+    public String get_email() {
+        return this.email;
+    }
+
+    public ArrayList<Student> get_students() {
+        return this.students;
+    }
+    
+    public ArrayList<Course> get_courses() {
+        return this.courses;
+    }
+
+    //defining the setters
+
+    public void set_id(int id) {
+        this.teacher_id = id;
+    }
+
+    public void set_firstname(String fn) {
+        this.first_name = fn;
+    }
+
+    public void set_lastname(String ln) {
+        this.last_name = ln;
+    }
+
+    public void set_password(String pwd) {
+        this.password = pwd;
+    }
+
+    public void set_email(String mail) {
+        this.email = mail;
+    }
+
+    public void add_student(Student s) {
+        this.students.add(s);
+    }
+
+    public void add_course(Course c) {
+        this.courses.add(c);
+    }
+}
